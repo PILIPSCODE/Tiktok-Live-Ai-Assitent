a707c009c3b0964f092a99decb058793f8b62911
Pilipus <Pilipus@gmail.com>
2025-01-08 10:15:53 +0700

    fix: try to fix play music

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.get("/reqMusic", async (req, res) => {
 
     await fsExtra.ensureDir(audioDir);
     await execCommand(
-      `${ytDlpPath} --cookies cookies.json -x --audio-format mp3 --ffmpeg-location "${ffmpegPath}" -o "${audioPath}" "https://www.youtube.com/watch?v=${video.id}"`
+      `${ytDlpPath} --cookies cookies.txt -x --audio-format mp3 --ffmpeg-location "${ffmpegPath}" -o "${audioPath}" "https://www.youtube.com/watch?v=${video.id}"`
     );
 
     const data = fs.readFileSync(audioPath);
