9c12946451050a9e12d086dcf6af0636611a4b21
Pilipus <Pilipus@gmail.com>
2025-02-07 11:38:46 +0700

    fix: fix cookies

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.get("/reqMusic", async (req, res) => {
 
     await fsExtra.ensureDir(audioDir);
     await execCommand(
-      `${ytDlpPath} --cookies-from-browser chrome --cookies cookies.txt -x --audio-format mp3 --ffmpeg-location "${ffmpegPath}" -o "${audioPath}" "https://www.youtube.com/watch?v=${video.id}"`
+      `${ytDlpPath} -cookies cookies.txt -x --audio-format mp3 --ffmpeg-location "${ffmpegPath}" -o "${audioPath}" "https://www.youtube.com/watch?v=${video.id}"`
     );
 
     const data = fs.readFileSync(audioPath);
